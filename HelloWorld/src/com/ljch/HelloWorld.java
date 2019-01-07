c54d83cb6c0f684476ff2c8dfce80da0be80cc40
ljch <ljch0403@163.com>
2019-01-07 16:34:46 +0800

    Initial commit

---
+++ HelloWorld/src/com/ljch/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ljch;
+
+/**
+ * 学习java用
+ * Created by ljch0 on 2019/1/7.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("这是我第二次学习java");
+    }
+}
