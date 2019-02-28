6ad01198af87ce973b0f9bcb0fc41e3611d09cf7
ljch <ljch0403@163.com>
2019-02-28 16:14:42 +0800

    添加junit单元测试

--- src/main/java/com/ljch/HelloWorld.java
+++ src/main/java/com/ljch/HelloWorld.java
@@ -5,7 +5,8 @@ package com.ljch;
  * Created by ljch0 on 2019/1/7.
  */
 public class HelloWorld {
-    public static void main(String[] args){
+
+    public void println(){
         System.out.println("这是我第二次学习java");
     }
 }
