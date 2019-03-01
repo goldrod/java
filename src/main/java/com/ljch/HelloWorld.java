7cc9d928ba107c6f8fbae0d384c6e92ecd1af094
ljch <ljch0403@163.com>
2019-03-01 10:13:47 +0800

    添加junit单元测试

--- src/main/java/com/ljch/HelloWorld.java
+++ src/main/java/com/ljch/HelloWorld.java
@@ -1,12 +1,22 @@
 package com.ljch;
 
+import org.junit.Test;
+
 /**
  * 学习java用
  * Created by ljch0 on 2019/1/7.
  */
 public class HelloWorld {
 
+    @Test
     public void println(){
+        String str = "你好";
+        StringBuffer stringBuffer = new StringBuffer();
         System.out.println("这是我第二次学习java");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
 }
